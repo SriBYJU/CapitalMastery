ed1f975278f46f9afabadf2ae5fdb7cc80dbb0cf
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2026-08-27 22:13:48 -0400

    Add Capital Mastery Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.CAPITAL_MASTERY_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCwaHa6Xor21670G0t40IHZzBSos9y-HBw",
+  authDomain: "capital-mastery26.firebaseapp.com",
+  projectId: "capital-mastery26",
+  storageBucket: "capital-mastery26.firebasestorage.app",
+  messagingSenderId: "13730226275",
+  appId: "1:13730226275:web:cff8d0d5bb8b87ad3ed856",
+  measurementId: "G-GP2LQY6W0L"
+};
+
+window.CAPITAL_MASTERY_API_URL = "https://capital-mastery-api.avadhanula-shriyan.workers.dev";
