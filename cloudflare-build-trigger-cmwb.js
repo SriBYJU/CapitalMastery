076936b8fa155ea640767a374e413d4870ca710d
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2026-09-08 18:27:03 -0400

    Trigger Cloudflare Worker build after reconnect

--- cloudflare-build-trigger-cmwb.js
+++ cloudflare-build-trigger-cmwb.js
@@ -1,3 +1,3 @@
 // Capital Mastery Cloudflare Workers Builds trigger after CMWB token configuration.
 // This file is intentionally not imported by the application.
-export const capitalMasteryCloudflareBuildTrigger = '2026-09-08T22:15Z';
+export const capitalMasteryCloudflareBuildTrigger = '2026-09-08T22:31Z';
